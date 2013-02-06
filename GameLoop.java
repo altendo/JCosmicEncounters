345e01b63b23a549491b5db7556121daad5d1db3
Morgan Harold Bauer <bauer.morgan@gmail.com>
2013-02-05 22:55:25 -0600

    put the game phases in a file

---
+++ GameLoop.java
@@ -0,0 +1,19 @@
+class GameLoop
+{
+    enum gamePhases {
+        Regroup, Destiny, Launch, Alliance, Planning, Reveal, Resolution,
+    };
+
+    public static void main(String[] a)
+    {
+        // get the number of players
+        System.out.print("How many players are there");
+        int players = 5;
+        // create players
+        // five planets per player
+        // four ships per planet (20 total)
+        while (true)
+            {
+            }
+    }
+}
