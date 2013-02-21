37432e04a8466f0fe9b463a1309917a754f1edd3
Morgan Harold Bauer <bauer.morgan@gmail.com>
2013-02-20 23:17:03 -0600

    house rules starter
    Signed-off-by: Morgan Harold Bauer <bauer.morgan@gmail.com>

---
+++ src/main/java/org/mhb/suckitpaul/HouseRules.java
@@ -0,0 +1,8 @@
+package org.mhb.suckitpaul;
+
+public class HouseRules {
+	// a place to put the house rules that we want to make available
+	// this should contain the places where stuff needs to change and what needs to chane to to make stuff behave differently
+	
+	// one thing could be the extra players that we might want above and beyond the maximum five
+}
