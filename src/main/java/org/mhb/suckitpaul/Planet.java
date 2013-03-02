0238780eb3aea962336946b67de50afff957ed19
Morgan Harold Bauer <bauer.morgan@gmail.com>
2013-03-01 23:08:05 -0600

    comments!
    Signed-off-by: Morgan Harold Bauer <bauer.morgan@gmail.com>

--- src/main/java/org/mhb/suckitpaul/Planet.java
+++ src/main/java/org/mhb/suckitpaul/Planet.java
@@ -7,10 +7,23 @@ public class Planet {
  * suggest map of players to number of ships they have on a planet
  */
   private Map<Player, Integer> occupiers;
+  /**
+   * Constructors
+   */
+  
+  /**
+   * The player to 
+   * @param p
+   */
   public Planet(Player p)
   {
     occupiers.put(p, 4);
   }
+  /**
+   * The player to create the given number of planets for.
+   * @param p
+   * @param startingPresence
+   */
   public Planet(Player p, int startingPresence)
   {
     occupiers.put(p, startingPresence);
