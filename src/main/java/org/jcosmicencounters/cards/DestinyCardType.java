3e958d9e4424e499bb570560004a3df51d0a854e
Morgan Harold Bauer <bauer.morgan@gmail.com>
2013-03-07 20:24:36 -0600

    comments on destinycardtype and my rationale for where I was going

--- src/main/java/org/jcosmicencounters/cards/DestinyCardType.java
+++ src/main/java/org/jcosmicencounters/cards/DestinyCardType.java
@@ -1,6 +1,11 @@
 package org.jcosmicencounters.cards;
 
-public enum DestinyCardType 
+/**
+ * Nick, I was trying to avoid hardcoding the colors, because a) they don't matter b) I want this to be extensible & c) I'd like to be able to remap them client side, so anyone can have it look the way they want.
+ * as far as (a) goes, what's important is the player being identified, which is why I gave the destiny card the player when I instantiated it.
+ * for (b) we can try to create a version with more than five players.
+ */
+public enum DestinyCardType
 {
     RED,
     BLUE,
